52d7da63f0b13e3dbd67ff46bd55be8942a363c2
Jon Rogers <jon@carbonfive.com>
2020-11-14 19:19:11 -0800

    remove js link listener

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -7,35 +7,4 @@ function attachPrintListener() {
     });
 }
 
-// fix up links for github pages deployment
-function directoryDepth(path) {
-  return path.split("/").length - 1;
-}
-
-function attachPastLinkListener() {
-  if (window.location.pathname === "/") {
-    // we're at root - no need to fix links
-    return;
-  }
-  const curPath = window.location.pathname;
-  document
-    .querySelectorAll(".past-issues__link[href], .nav-item__info-link[href]")
-    .forEach(function (link) {
-      const depth = directoryDepth(curPath);
-      if (depth > 0) {
-        const directories = curPath.split("/").slice(1, depth - 1);
-        directories.push(link.getAttribute("href"));
-        let newPath = "/" + directories.join("/");
-        if (newPath === "//") {
-          newPath = "/";
-        }
-        link.addEventListener("click", function (ev) {
-          ev.preventDefault();
-          window.location.href = newPath;
-        });
-      }
-    });
-}
-
 document.addEventListener("DOMContentLoaded", attachPrintListener);
-document.addEventListener("DOMContentLoaded", attachPastLinkListener);
